1e9115ddff2b017a53f24ea94e0c87624b949dc3
Jachimike Onuoha <jachiuzo@gmail.com>
2024-08-31 13:09:21 -0700

    Setup letterMap

---
+++ puzzle-solver.ts
@@ -0,0 +1,34 @@
+interface Coordinate {
+    row: number
+    col: number
+};
+type LetterMap = { [key: string]: number };
+
+class WordPuzzleSolver {
+    private wordPuzzle: string[][] = [['C','B','T'],['A','R','M'],['T','A','V']];
+    private wordsList: string[] = ["CAT","ARM","ART"]
+    private letterMap: LetterMap;
+
+    constructor(){
+        this.letterMap = this.initializeLetterMap();
+    };
+
+    private initializeLetterMap(): LetterMap {
+        let resultLetterMap: LetterMap =  {};
+        let letterArr = [...Array(26)].map( i => String.fromCharCode(i+39) )
+        for(const letter in letterArr){
+            resultLetterMap[letter] = 0
+        }
+        return resultLetterMap;
+    }
+
+    private populateLetterMapFor(puzzle: string[][]) {
+        let resultMap: LetterMap;
+        for(const row of puzzle){
+            for(const letter of row){
+                // Add the coord value of the letter to the appropriate key (the letter itself)
+            };
+        };
+    };
+
+};
