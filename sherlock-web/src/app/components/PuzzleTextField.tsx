dd7a6d24f5adbfc0a1105a7748f560e3b5d2e5b4
Jachimike Onuoha <jachiuzo@gmail.com>
2024-09-20 22:38:02 -0700

    Improved UI to presentable state

---
+++ sherlock-web/src/app/components/PuzzleTextField.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { TextField } from '@fluentui/react';
+
+export default function PuzzleTextField() {
+    return (
+    <TextField
+        placeholder='Enter puzzle image url'
+    />
+    )
+}
--- sherlock-web/src/app/page.tsx
+++ sherlock-web/src/app/page.tsx
@@ -1,18 +1,17 @@
 import dynamic from "next/dynamic";
 import Image from "next/image";
+import sherlockLogo from "./assets/sherlock-logo.jpg";
 
+const PuzzleTextField = dynamic(()=> import("./components/PuzzleTextField"), {ssr:false})
 const SubmitButton = dynamic(()=> import("./components/SubmitButton"), {ssr:false})
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-center">
         <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src={sherlockLogo}
+          alt="Detective hat and magnifying glass"
           priority
         />
 
@@ -28,6 +27,7 @@ export default function Home() {
         </ol> */}
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
+            <PuzzleTextField/>
             <SubmitButton/>
           {/* <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
